ccbd780f2a86c89773f346d81bfd7e2f939c4e0b
joshuavaughn <school.vaughnjoshua@gmail.com>
2025-03-04 13:21:55 +0800

    fixed complete tasks trigger

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ document.querySelector("#addTask").addEventListener("click", () => {
 
     document.querySelector("#taskList").innerHTML += `
     <li class="list-group-item d-flex align-items-center gap-3">
-        <span class="material-symbols-outlined me-4" style="color: lightgray">task_alt</span>
-
+        <button class="btn d-flex justify-content-center align-items-center completeTask">
+            <span class="material-symbols-outlined me-4" style="color: lightgray">task_alt</span>
+        </button>
         <h3 id="task">${task}</h3>
         <small>${description}</small>
-        <button class="btn btn-light closeTask ms-auto">
+        <button class="btn d-flex justify-content-center align-items-center closeTask ms-auto">
             <span class="material-symbols-outlined">close</span>
         </button>
     </li>`;
@@ -25,15 +26,20 @@ document.querySelector("#addTask").addEventListener("click", () => {
     const myModal = document.querySelector("#closeModal");
     myModal.click();
 
-    const tasks = document.querySelectorAll(".closeTask");
-    tasks.forEach((task) => {
+    const closeTasks = document.querySelectorAll(".closeTask");
+    closeTasks.forEach((task) => {
+        task.addEventListener("click", (e) => {
+            e.target.closest("li").remove();
+        })
+    })
+
+    const completeTasks = document.querySelectorAll(".completeTask");
+    completeTasks.forEach((task) => {
         task.addEventListener("click", (e) => {
             let grandparent  = e.target.closest("li");
 
             let currentTask = grandparent.children[1].innerHTML;
             let currentTaskDescription = grandparent.children[2].innerHTML;
-
-            e.target.closest("li").remove();
 
             completed (currentTask, currentTaskDescription);
         })
@@ -44,11 +50,11 @@ document.querySelector("#addTask").addEventListener("click", () => {
 function completed (task, description) {
     document.querySelector("#CompletedTaskList").innerHTML += `
     <li class="list-group-item d-flex align-items-center gap-3">
-        <span class="material-symbols-outlined me-4" style="color: green">task_alt</span>
+        <span class="material-symbols-outlined ms-3 me-4" style="color: green">task_alt</span>
 
         <h3 id="task" style="text-decoration: line-through;">${task}</h3>
         <small style="text-decoration: line-through;">${description}</small>
-        <button class="btn btn-light closeTask ms-auto">
+        <button class="btn d-flex justify-content-center align-items-center closeTask ms-auto">
             <span class="material-symbols-outlined">close</span>
         </button>
     </li>`;
